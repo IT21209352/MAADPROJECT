7dd082b8864d4938c0ae4a5c0283b90fd3164e69
it21296796 <IT21296796@my.sliit.lk>
2023-05-03 20:27:26 +0530

    upload

--- app/src/main/java/com/example/helpinghand/Post.kt
+++ app/src/main/java/com/example/helpinghand/Post.kt
@@ -1,6 +1,13 @@
 package com.example.helpinghand
 
+<<<<<<< HEAD
 data class Post( val postId: String = "",
                  val postDetail: String? = null,
                  val imageUrl: String? = null ,
                  )
+=======
+data class Post(
+    var postId: String? = null,
+    val postDetail: String? = null,
+    val imageUrl: String? = null)
+>>>>>>> 2764fc5 (upload)
