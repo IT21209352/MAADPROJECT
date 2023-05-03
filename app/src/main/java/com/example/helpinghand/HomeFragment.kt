17b71d37de30b5663f2ee2756eb96a6c89756b95
IT21209352 <it21209352@my.sliit.lk>
2023-05-03 18:20:39 +0530

    add comment send

--- app/src/main/java/com/example/helpinghand/HomeFragment.kt
+++ app/src/main/java/com/example/helpinghand/HomeFragment.kt
@@ -1,19 +1,30 @@
 package com.example.helpinghand
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 
 class HomeFragment : Fragment() {
 
     private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseFirestore
+    private var textCollectionRef: CollectionReference? = null
+    private lateinit var user: FirebaseUser
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -29,6 +40,9 @@ class HomeFragment : Fragment() {
 
         val logoutBtn = view.findViewById<Button>(R.id.main_logout_btn)
 
+        val cmntBtn= view.findViewById<Button>(R.id.cmntBtn)
+
+
         logoutBtn.setOnClickListener {
 
             Firebase.auth.signOut()
@@ -40,6 +54,35 @@ class HomeFragment : Fragment() {
 
         }
 
+        cmntBtn.setOnClickListener {
+
+            val cmntInput = view.findViewById<EditText>(R.id.cmntInput)
+            val texts: String = cmntInput.getText().toString()
+            db = FirebaseFirestore.getInstance();
+            textCollectionRef = db!!.collection("comments")
+
+            val user =  auth.currentUser?.email.toString()
+
+            Toast.makeText(activity, user, Toast.LENGTH_SHORT).show()
+
+            val textMap = hashMapOf(
+                "comments-comment" to texts,
+                "comments-owner" to user
+
+            )
+            textCollectionRef!!
+                .add(textMap)
+
+                .addOnSuccessListener { documentReference ->
+                    Toast.makeText(activity, "Comment has been saved...", Toast.LENGTH_SHORT).show()
+                    Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+                    cmntInput.setText("")
+                }
+                .addOnFailureListener { e ->
+                    Toast.makeText(activity, "Error Occurred", Toast.LENGTH_SHORT).show()
+                    Log.w(TAG, "Error adding document", e)
+                }
+        }
 
         return view
     }
